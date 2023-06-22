86ca851965dcb899e17cb0f46969f108845f7629
Kwanjeera W <kwanjeeraw@gmail.com>
2023-06-22 23:52:24 +0700

    edits

--- ui.R
+++ ui.R
@@ -1,11 +1,6 @@
 library(shiny)
 ui <- fluidPage(theme = bs_theme(version = 3, bootswatch = "lumen", base_font = font_google("Nunito"),
                                  code_font = font_google("Space Mono")),
-                # tags$head(
-                #   tags$style(
-                #     HTML(".shiny-notification {position:fixed;top: calc(90%);left: calc(1%);}")
-                #   )
-                # ),
                 shinyjs::useShinyjs(),
                 shinyjs::extendShinyjs(text = app_jscode, functions = c('disableTab','enableTab')),
                 extendShinyjs(text = reset_all_evenData, functions = c("resetSelected")),
