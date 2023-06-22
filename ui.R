1bc21ad1d46e67bb79a66e441a315a5d74666461
Kwanjeera W <kwanjeeraw@gmail.com>
2023-06-22 23:50:54 +0700

    edits

--- ui.R
+++ ui.R
@@ -1,3 +1,4 @@
+library(shiny)
 ui <- fluidPage(theme = bs_theme(version = 3, bootswatch = "lumen", base_font = font_google("Nunito"),
                                  code_font = font_google("Space Mono")),
                 # tags$head(
@@ -8,7 +9,7 @@ ui <- fluidPage(theme = bs_theme(version = 3, bootswatch = "lumen", base_font = 
                 shinyjs::useShinyjs(),
                 shinyjs::extendShinyjs(text = app_jscode, functions = c('disableTab','enableTab')),
                 extendShinyjs(text = reset_all_evenData, functions = c("resetSelected")),
-                navbarPage(windowTitle="Metabox 2.0",img(src="www/metabox2.png",width=105), id="tabs",
+                navbarPage(windowTitle="Metabox 2.0",img(src="/www/metabox2.png",width=105), id="tabs",
                 )## navbarPage ##
 
 )
