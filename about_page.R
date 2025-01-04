6205ca42c31533a243a663fa3c9c36c68c25588a
Kwanjeera W <kwanjeeraw@gmail.com>
2025-01-04 15:26:35 +0700

    update about page

--- about_page.R
+++ about_page.R
@@ -1,6 +1,11 @@
 fluidRow(
   column(12,
-         h2("About"),hr(style="border-top: 1px dashed #B5C7DA;"),
+         fluidRow(column(5,h2("About")),
+                  column(7,align="right",
+                         tags$div(tags$a(href="https://www.si.mahidol.ac.th/",tags$img(src="mulogo.png")),
+                                  tags$a(href="https://www.si.mahidol.ac.th/th/research-academics/research/simpc/",tags$img(src="simpclogo.png")),
+                                  tags$a(href="http://metsysbio.com/",tags$img(src="msblogo.png")))
+                  )),hr(style="border-top: 1px dashed #B5C7DA;"),
          fluidRow(
            h4("Metabox 2.0"),
            column(12,
@@ -16,42 +21,32 @@ fluidRow(
          fluidRow(
            h4("Updates"),
            column(12,
-                  HTML('<h5>version 2.6 (JUNE 2023)</h5>
-                  <ul><li>Change report location</li>
-                  <li>Set default package color</li>
-                  <li>Update MUVR to current version</li>
-                  <li>Fix bug when running univariate analysis</li></ul>
-                  <h5>version 2.5 (MAY 2023)</h5>
-                  <ul><li>Update pathway data for enrichment analysis</li></ul>
-                  <h5>version 2.4 (MAR 2023)</h5>
-                  <ul><li>Add example data sets for GUI version</li></ul>
-                  <h5>version 2.3 (FEB 2023)</h5>
-                  <ul><li>Summarize coefficient of variation (cv) and normality</li>
-                  <li>Fix default scaling of PCA plot</li>
-                  <li>Fix default color</li></ul>
-                  <h5>version 2.2 (OCT 2022)</h5>
-                  <ul><li>Fix bug when running MUVR</li></ul>
-                  <h5>version 2.1 (SEP 2022)</h5>
-                  <ul><li>Add imputation methods: zero, half-min</li>
-                  <li>Allow scaling and block weighting by the block inertia</li>
-                  <li>Allow missing values in normalization, transformation and scaling</li></ul>
-                  <h5>version 2.0 (JUL 2022)</h5>
-                  <ul><li>Initial release</li></ul>'),
+                  HTML('<h5>version 2.11 (DEC 2024)</h5>
+                  <ul><li>New combine_statplot function for command version</li>
+                  <li>Fix bugs for impute missing values in GUI version</li></ul>
+                  <h5>version 2.10 (NOV 2024)</h5>
+                  <ul><li>New multiv_viploadingplot function</li>
+                  <li>New scoreplot with shape for command version</li>
+                  <li>New impute missing values by LOD and PCA overview</li>
+                  <li>Update annotation data</li>
+                  <li>Remove test_multinormality and test_multiequalvar functions</li>
+                  <li>Change to glog2 for rlaplot</li>
+                  <li>Change default no. of predI to 5</li>
+                  <li>Change default ptsize to 3</li>
+                  <li>Change pdf size of a report</li>
+                  <li>Change transparency of score plot</li>
+                  <li>Update and bugs fix for GUI version</li>
+                  <li>Fix bugs for enrichment and ORA analysis</li></ul>
+                  <p><a href="https://github.com/kwanjeeraw/metabox2">See all updates</a>.</p>'),
            )),hr(style="border-top: 1px dashed #B5C7DA;"),
          fluidRow(
-           h5("References"),
+           h4("References"),
            column(12,
                   tags$p(a(href="https://github.com/kwanjeeraw/metabox2","Metabox 2.0"),
                          "- A toolbox for thorough metabolomic data analysis, integration and interpretation, see",
-                         a(href="#","[Ref]")),
+                         a(href="https://academic.oup.com/gigascience/article/doi/10.1093/gigascience/giae005/7629842","[Ref]")),
                   tags$p(a(href="https://github.com/kwanjeeraw/metabox","Metabox 1.0"),
                          "- A toolbox for metabolomic data analysis, interpretation and integrative exploration was released in 2016, see",
                          a(href="https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0171046","[Ref]"))
-           )),hr(),
-         fluidRow(
-           column(12,align="center",
-                  tags$div(tags$a(href="https://www.si.mahidol.ac.th/",tags$img(src="mulogo.png")),
-                  tags$a(href="https://www.si.mahidol.ac.th/th/research-academics/research/simpc/",tags$img(src="simpclogo.png")),
-                  tags$a(href="http://metsysbio.com/",tags$img(src="msblogo.png")))
-          ))
+           ))
   ))
